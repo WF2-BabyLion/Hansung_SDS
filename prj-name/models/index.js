431ed47fab58f8c15611f3e9a23894cda85bab46
yjh-1008 <exesea12@gmail.com>
2022-05-19 02:03:42 +0900

    좋아요 구현

--- prj-name/models/index.js
+++ prj-name/models/index.js
@@ -4,7 +4,7 @@ const config = require('../config/config')[env];
 const User = require('./user');
 const Post = require('./post');
 const Hashtag = require('./hashtag');
-
+const Likes =  require('./like.js');
 const db = {};
 const sequelize = new Sequelize(
   config.database, config.username, config.password, config,
@@ -14,13 +14,15 @@ db.sequelize = sequelize;
 db.User = User;
 db.Post = Post;
 db.Hashtag = Hashtag;
+db.Likes = Likes;
 
 User.init(sequelize);
 Post.init(sequelize);
 Hashtag.init(sequelize);
-
+Likes.init(sequelize);
 User.associate(db);
 Post.associate(db);
 Hashtag.associate(db);
+Likes.associate(db)
 
 module.exports = db;
--- prj-name/routes/user.js
+++ prj-name/routes/user.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const { isLoggedIn } = require('./middlewares');
 const User = require('../models/user');
+const Post = require('../models/post');
+const Likes = require('../models/like');
 
 const router = express.Router();
 
 router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
+  //console.log(req.user)
   try {
     const user = await User.findOne({ where: { id: req.user.id } });
     if (user) {
+      console.log("User : ",user)
       await user.addFollowing(parseInt(req.params.id, 10));
       res.send('success');
     } else {
@@ -20,4 +24,70 @@ router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
   }
 });
 
+router.post('/:id/like', isLoggedIn, async (req, res, next) => {
+  //console.log(req.params)
+  const {UserId, PostId}= await req.body;
+  try {
+    console.log(UserId, PostId)
+    if(UserId !== null && PostId!==null){
+      await Likes.create(
+        {
+        UserId,
+        PostId,
+        }
+      )
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/check', isLoggedIn, async (req, res, next) => {
+  //console.log(req.params)
+  const {UserId, PostId}= await req.body;
+  try {
+    const LikeFindAll = Likes.findAll({
+      where:{
+        UserId:UserId
+      }
+    }).then((result)=>{
+      console.log(result);
+      var chk;
+      result.map((data)=>{
+        console.log(data.dataValues.PostId,"      ",PostId)
+        if(data.dataValues.PostId===PostId){
+          chk=true;
+        }
+      })
+      if(chk){
+        res.send("true");
+      }else{
+        res.send("false");
+      }
+    })
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/:id/delete', isLoggedIn, async (req, res, next) => {
+  //console.log(req.params)
+  const {UserId, PostId}= await req.body;
+  try {
+    if(UserId !== null && PostId!==null){
+      await Likes.destroy({
+        where:{
+          UserId:UserId,
+          PostId:PostId,
+        }
+      }
+      )
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 module.exports = router;
