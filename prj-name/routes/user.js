e1f1f249ef8b920c9bdd6adf6978f30268e460fc
LINKER <temp2temp@daum.net>
2022-05-21 04:41:34 +0900

    팔로우 취소 구현 및 CSS 수정

--- prj-name/routes/user.js
+++ prj-name/routes/user.js
@@ -24,6 +24,21 @@ router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
   }
 });
 
+//팔로우 취소
+router.post('/:id/unfollow', isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { id: req.user.id } });
+    if (user) {
+      await user.removeFollowing(parseInt(req.params.id, 10));
+      res.send('success');
+    } else {
+      res.status(404).send('no user');
+    }
+  } catch (err) {
+    console.dir(err);
+  }
+});
+
 router.post('/:id/like', isLoggedIn, async (req, res, next) => {
   //console.log(req.params)
   const { UserId, PostId } = await req.body;
