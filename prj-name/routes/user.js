39d4bd202285531c80184c5f4ce443d48918e565
LINKER <temp2temp@daum.net>
2022-05-21 03:52:02 +0900

    닉네임 실시간 변경 기능 추가 (프로필 링크에서만 보임)

--- prj-name/routes/user.js
+++ prj-name/routes/user.js
@@ -12,7 +12,7 @@ router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
   try {
     const user = await User.findOne({ where: { id: req.user.id } });
     if (user) {
-      console.log("User : ",user)
+      console.log('User : ', user);
       await user.addFollowing(parseInt(req.params.id, 10));
       res.send('success');
     } else {
@@ -26,16 +26,14 @@ router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
 
 router.post('/:id/like', isLoggedIn, async (req, res, next) => {
   //console.log(req.params)
-  const {UserId, PostId}= await req.body;
+  const { UserId, PostId } = await req.body;
   try {
-    console.log(UserId, PostId)
-    if(UserId !== null && PostId!==null){
-      await Likes.create(
-        {
+    console.log(UserId, PostId);
+    if (UserId !== null && PostId !== null) {
+      await Likes.create({
         UserId,
         PostId,
-        }
-      )
+      });
     }
   } catch (error) {
     console.error(error);
@@ -45,27 +43,27 @@ router.post('/:id/like', isLoggedIn, async (req, res, next) => {
 
 router.post('/check', isLoggedIn, async (req, res, next) => {
   //console.log(req.params)
-  const {UserId, PostId}= await req.body;
+  const { UserId, PostId } = await req.body;
   try {
     const LikeFindAll = Likes.findAll({
-      where:{
-        UserId:UserId
-      }
-    }).then((result)=>{
+      where: {
+        UserId: UserId,
+      },
+    }).then((result) => {
       console.log(result);
       var chk;
-      result.map((data)=>{
-        console.log(data.dataValues.PostId,"      ",PostId)
-        if(data.dataValues.PostId===PostId){
-          chk=true;
+      result.map((data) => {
+        console.log(data.dataValues.PostId, '      ', PostId);
+        if (data.dataValues.PostId === PostId) {
+          chk = true;
         }
-      })
-      if(chk){
-        res.send("true");
-      }else{
-        res.send("false");
+      });
+      if (chk) {
+        res.send('true');
+      } else {
+        res.send('false');
       }
-    })
+    });
   } catch (error) {
     console.error(error);
     next(error);
@@ -74,20 +72,36 @@ router.post('/check', isLoggedIn, async (req, res, next) => {
 
 router.post('/:id/delete', isLoggedIn, async (req, res, next) => {
   //console.log(req.params)
-  const {UserId, PostId}= await req.body;
+  const { UserId, PostId } = await req.body;
   try {
-    if(UserId !== null && PostId!==null){
+    if (UserId !== null && PostId !== null) {
       await Likes.destroy({
-        where:{
-          UserId:UserId,
-          PostId:PostId,
-        }
-      }
-      )
+        where: {
+          UserId: UserId,
+          PostId: PostId,
+        },
+      });
     }
   } catch (error) {
     console.error(error);
     next(error);
+  }
+});
+
+//닉네임 수정
+router.post('/change', isLoggedIn, async (req, res, next) => {
+  const changeName = req.body.name;
+  try {
+    const user = await User.findOne({ where: { id: req.user.id } });
+    if (user) {
+      User.update({ nick: changeName }, { where: { id: req.user.id } });
+      return res.send('닉네임 변경 완료');
+    } else {
+      res.status(404).send('유저가 없습니다.');
+    }
+  } catch (err) {
+    console.error(err);
+    next(err);
   }
 });
 module.exports = router;
